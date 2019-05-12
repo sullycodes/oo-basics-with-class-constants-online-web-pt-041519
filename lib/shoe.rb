6d5645b37681dad7a64950d7302399dff145397a
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-12 16:52:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,11 +3,11 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
-  BRANDS = BRANDS.uniq
 
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    BRANDS.uniq
   end
 
   def cobble
